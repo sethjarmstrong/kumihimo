a5b1abab773a5309b8597a0aee8425840429fee1
Seth Armstrong <seth.armstrong@fluxfederation.com>
2019-02-05 20:55:25 +1300

    Revert "Revert "Made braided visualizer offset like the 3D visualizer""
    This reverts commit 295349b257da5238f3971b41cfc17ad7dbdf4100.

--- js/braided_visualizer.js
+++ js/braided_visualizer.js
@@ -12,6 +12,10 @@ class BraidedVisualizer extends Visualizer {
     return this.braid.numThreads / 2;
   }
 
+  get width() {
+    return ((this.columns + 0.5) * this.px_per_bead) + 'px';
+  }
+
   render() {
     this.loom.weave();
     super.render();
@@ -25,7 +29,7 @@ class BraidedVisualizer extends Visualizer {
 
     for (var i = 0; i < this.loom.beads.length; i++) {
       var y = (row_width * i + row_mid) + '%';
-      var row_svgs = this.row_svg(this.loom.beads[i], y);
+      var row_svgs = this.row_svg(this.loom.beads[i], y, i % 2 === 1);
 
       row_svgs.forEach(function(svg) {
         elements.push(svg);
@@ -35,14 +39,15 @@ class BraidedVisualizer extends Visualizer {
     return elements;
   }
 
-  row_svg(row, y) {
+  row_svg(row, y, use_offset) {
     var elements = [];
 
-    var column_width = 100 / row.length;
+    var column_width = this.px_per_bead;
     var column_mid = column_width / 2;
 
     for (var i = 0; i < row.length; i++) {
-      var x = (column_width * i + column_mid) + '%';
+      var offset = (use_offset ? column_mid : 0);
+      var x = column_width * i + column_mid + offset;
       elements.push(this.bead_svg(row[i], x, y));
     }
 
