c37d04f7f2a0bdba9de07667a571bf9fc1587556
Seth Armstrong <seth.armstrong@fluxfederation.com>
2019-06-13 10:37:44 +1200

    Removed the 2D braided visualizer

--- js/init.js
+++ js/init.js
@@ -12,9 +12,9 @@
       type: 'row',
       content: [
         {
-          title: '2D Braided View',
+          title: 'Stringing Guide (Names)',
           type: 'component',
-          componentName: '2d-braided-visualizer',
+          componentName: 'printable-stringing-visualizer',
           componentState: {}
         },
         {
@@ -117,7 +117,6 @@
       };
     }
 
-    layout.registerComponent('2d-braided-visualizer', registration_function(BraidedVisualizer));
     layout.registerComponent('3d-braided-visualizer', registration_function(ThreeDVisualizer));
     layout.registerComponent('unbraided-visualizer', registration_function(UnbraidedVisualizer));
     layout.registerComponent('stringing-visualizer', registration_function(StringingVisualizer));
@@ -139,7 +138,6 @@
       layout.createDragSource(element, new_item_config);
     };
 
-    add_menu_item('2d-braided-visualizer', '2D Braided View');
     add_menu_item('3d-braided-visualizer', '3D Braided View');
     add_menu_item('unbraided-visualizer', 'Unbraided View');
     add_menu_item('stringing-visualizer', 'Stringing Guide (Colours)');
